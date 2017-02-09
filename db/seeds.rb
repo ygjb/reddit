155eb5ceddac5a0d7a1769bd64218e4db6c81021
Andrij Tymchenko <xtymchenko@ex.ua>
2017-02-09 14:59:27 +0000

    seed new object

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,10 @@
+user = User.new(
+      :email                 => 'xtymchenko@ex.ua',
+      :password              => 'Andreylol99!',
+      :password_confirmation => 'Andreylol99!'
+)
+user.skip_confirmation!
+user.save!
 20.times do
   pass = Faker::Internet.password(10, 20, true, true)
   user = User.new(
@@ -9,7 +16,7 @@
   user.save!
   rnd = Random.new
   rnd.rand(10..100).times do
-    Link.create!(
+    link = Link.create!(
       :title => Faker::Lorem.sentence(3, true),
       :url  => Faker::Internet.url,
       :user => user
