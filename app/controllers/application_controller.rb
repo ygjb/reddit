8ca9b7c6395467fcbe6bbf091a9a28c9db63064b
Andrij Tymchenko <xtymchenko@ex.ua>
2017-02-09 14:57:03 +0000

    check current_user method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  protected
+    def check_current_user?
+      redirect_to links_path unless current_user
+    end
 end
