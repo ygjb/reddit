3789b073fd7f408505827c52a937dafdcd87a9ba
Andrij Tymchenko <xtymchenko@ex.ua>
2017-02-23 19:58:57 +0000

    fix heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -74,13 +74,20 @@ Rails.application.configure do
   # Use a different logger for distributed setups.
   # require 'syslog/logger'
   # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new 'app-name')
+  config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
 
   if ENV["RAILS_LOG_TO_STDOUT"].present?
     logger           = ActiveSupport::Logger.new(STDOUT)
     logger.formatter = config.log_formatter
     config.logger = ActiveSupport::TaggedLogging.new(logger)
   end
-
-  # Do not dump schema after migrations.
+  
+  
+  # if ENV["RAILS_LOG_TO_STDOUT"].present?
+  #   logger           = ActiveSupport::Logger.new(STDOUT)
+  #   logger.formatter = config.log_formatter
+  #   config.logger = ActiveSupport::TaggedLogging.new(logger)
+  # end
+  # # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 end
