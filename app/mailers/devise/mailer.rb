66ac5d16896f03aa2d95831c8649d426b1cb804b
Andrij Tymchenko <xtymchenko@ex.ua>
2017-02-25 00:06:22 +0000

    fix mailer

--- app/mailers/devise/mailer.rb
+++ app/mailers/devise/mailer.rb
@@ -4,7 +4,7 @@ class MyDeviseMailer < PostageApp::Mailer
   def confirmation_instructions(record, token, opts = {})
     postageapp_template 'my-signup-confirmation'
     postageapp_variables :email => record.email,
-                         :link  => confirmation_url(record, :confirmation_token => record.confirmation_token)
+                         :link  => confirmation_path(record, :confirmation_token => record.confirmation_token)
 
     devise_mail(record, :confirmation_instructions)
   end
@@ -12,7 +12,7 @@ class MyDeviseMailer < PostageApp::Mailer
   def reset_password_instructions(record, token, opts = {})
     postageapp_template 'my-password-reset'
     postageapp_variables :name  => record.name ||= record.email,
-                         :link  => password_url(record, :reset_password_token => record.reset_password_token)
+                         :link  => password_path(record, :reset_password_token => record.reset_password_token)
 
     devise_mail(record, :reset_password_instructions)
   end
@@ -20,7 +20,7 @@ class MyDeviseMailer < PostageApp::Mailer
   def unlock_instructions(record)
     postageapp_template 'my-unlock-instructions'
     postageapp_variables :name  => record.name ||= record.email,
-                         :link  => unlock_url(record, :unlock_token => record.unlock_token)
+                         :link  => unlock_path(record, :unlock_token => record.unlock_token)
 
     devise_mail(record, :unlock_instructions)
   end
