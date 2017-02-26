f091a32732e3781409d91c44558731d3dba06f33
Andrij Tymchenko <xtymchenko@ex.ua>
2017-02-26 11:51:31 +0000

    fix json registation

--- app/controllers/api/v1/registrations_controller.rb
+++ app/controllers/api/v1/registrations_controller.rb
@@ -1,7 +1,7 @@
 class Api::V1::RegistrationsController < Api::BaseApiController
   respond_to :json
   def create
-    user = User.new(params[:user])
+    user = User.new(user_params)  
     if user.save
       render json: user.as_json(auth_token: user.authentication_token, email: user.email), status: :created
       return
@@ -9,5 +9,8 @@ class Api::V1::RegistrationsController < Api::BaseApiController
       warden.custom_failure!
       render json: user.errors, status: :unprocessable_entity
     end
+  end
+  def user_params
+     params.require(:new_user).permit(:email, :password, :password_confirmation)
   end
 end
