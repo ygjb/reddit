8e151ca2728f71268b7e7575c62d9ebf5fadf592
Andrij Tymchenko <xtymchenko@ex.ua>
2017-03-07 18:53:18 +0000

    put in order

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -61,10 +61,7 @@ Rails.application.configure do
     user_name:            ENV['GMAIL_USERNAME'],
     password:             ENV['GMAIL_PASSWORD'],
     authentication:       'plain'
-    # enable_starttls_auto: true
   }
-  # ENV['GMAIL_USERNAME']
-  # ENV['GMAIL_PASSWORD']
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
