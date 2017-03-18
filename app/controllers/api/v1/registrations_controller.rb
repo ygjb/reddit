5a1014bd5532e401298879a4872fcf4293386ef5
Andrij Tymchenko <xtymchenko@ex.ua>
2017-03-18 22:12:36 +0000

    fix status code

--- app/controllers/api/v1/registrations_controller.rb
+++ app/controllers/api/v1/registrations_controller.rb
@@ -1,6 +1,7 @@
 class Api::V1::RegistrationsController < Api::BaseApiController
   respond_to :json
   def create
+    begin
     user = User.new(user_params)  
     if user.save
       render json: user.as_json(auth_token: user.authentication_token, email: user.email), status: :created
@@ -9,7 +10,12 @@ class Api::V1::RegistrationsController < Api::BaseApiController
       warden.custom_failure!
       render json: user.errors, status: :unprocessable_entity
     end
+    rescue => ex
+      logger.error ex.message
+      render json: {:error => ex.message}, status: 400
+    end
   end
+  
   private
     def user_params
       params.require(:new_user).permit(:email, :password, :password_confirmation)
