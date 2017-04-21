27e5a519efb94c87e383331cc3cb877553bb7b42
Andrij Tymchenko <xtymchenko@ex.ua>
2017-04-21 12:38:14 +0000

    fix dependents

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,20 +1,10 @@
 class User < ApplicationRecord
   acts_as_token_authenticatable
   
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable,   :registerable, :confirmable,
+  devise :database_authenticatable, :registerable, :confirmable, 
          :recoverable, :rememberable, :trackable,  :validatable
-         
-  # has_many :links
-  has_many :posts
-  has_many :likes
-  has_many :comments
-  
-  #has_one :avatar, :class_name => "Image", :as => :imageable, :dependent => :destroy
-
-  # def image_styles
-  #   { :thumb => "50x50>",:medium => "150x150>" }
-  # end
-  
+       
+  has_many :posts, dependent: :destroy
+  has_many :likes, dependent: :destroy
+  has_many :comments, dependent: :destroy
 end
